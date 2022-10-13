dde9ac25d83456c84d43c7ac0fe02eb004260893
oghodwhy <WTFisthis4>
2022-10-13 14:14:18 -0700

    Adding cbz page and reader heirarchy

---
+++ app/src/cbzReader.java
@@ -0,0 +1,16 @@
+import java.util.zip;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import cbzPage;
+
+Public static class cbrReader(){
+
+
+
+
+
+
+
+};
