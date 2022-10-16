decccdba6e62370198f8bfe97d76e469396ecdb0
YashsviG <girdhar.yashsvi@gmail.com>
2022-10-15 17:37:06 -0700

    fe dashboard

--- app/src/main/java/com/example/comicshack/MainActivity.java
+++ app/src/main/java/com/example/comicshack/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.comicshack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.viewPagerImageSlider);
+
+        List<SliderItem> sliderItems = new ArrayList<>();
+        sliderItems.add(new SliderItem(R.drawable.cover_1));
+        sliderItems.add(new SliderItem(R.drawable.cover_2));
+        sliderItems.add(new SliderItem(R.drawable.cover_3));
+        sliderItems.add(new SliderItem(R.drawable.cover_4));
+        sliderItems.add(new SliderItem(R.drawable.cover_5));
+
+        viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.35f + r + 0.15f);
+            }
+        });
+        viewPager2.setPageTransformer(compositePageTransformer);
     }
 }
