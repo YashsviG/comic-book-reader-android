6113b37899767607f07a6e9915ae5801d1635986
oghodwhy <moena94@hotmail.com>
2022-10-25 21:37:10 -0700

    Added stub for comic library container class with basic description. Added basic members and descriptions to Comic and ComicPage class.

--- app/src/main/java/com/comicshack/comic/Comic.java
+++ app/src/main/java/com/comicshack/comic/Comic.java
@@ -9,11 +9,22 @@ import java.util.Iterator;
 import java.util.List;
 
 import com.comicshack.tools.FileArchive;
+import com.comicshack.comic.ComicPage;
 
+/**
+ *  Comic is intended as the container class for comic books
+ *  Should be the class we use to get anything about a specific comic,
+ *  Should be kept in a comic library
+ *  should contain at least the info we need when browsing through different comics
+ *  should be used as the container for any cached data for a specific comic (thumbnails etc)
+ */
 public class Comic
 {
-
-
+    private String title;
+    private String filename;
+    private FileArchive archive;
+    private ComicPage currentpage;
+    //add more fields to match
 
 
 
